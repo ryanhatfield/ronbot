0e3db36ba49ee28c01003bc28c3b4b14ba7f5bcc
Ryan Hatfield <ryan@ryan.bio>
2015-12-16 09:08:17 -0600

    Add initial code

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	glack "github.com/get-go/glack"
+)
+
+type quote struct {
+	Quote string
+}
+
+var ronurl = "http://ron-swanson-quotes.herokuapp.com/quotes"
+var profileurl = flag.String("profile-url", "https://twitter.com/ronswansonsays/profile_image?size=normal", "Ron's profile picture URL. Can be slack emoji example ':ron-swanson:'.")
+var token = flag.String("token", os.Getenv("SLACK_TOKEN"), "Slack API token to use. Defaults to 'SLACK_TOKEN' environment variable.")
+var channel = flag.String("channel", "#random", "Slack channel to post to")
+
+func main() {
+	flag.Parse()
+
+	g := glack.New(*token)
+
+	r, _ := http.Get(ronurl)
+	defer r.Body.Close()
+
+	var q quote
+	body, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(body, &q)
+
+	g.Send(&glack.Message{Channel: *channel, Icon: *profileurl, Username: "Ron Swanson", Message: q.Quote})
+}
